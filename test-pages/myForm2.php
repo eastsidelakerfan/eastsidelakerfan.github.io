f3027a90eed19d901a297c762c043ff107569c3b
eastsidelakerfan <54191400+eastsidelakerfan@users.noreply.github.com>
2021-03-07 19:05:03 -0500

    update

---
+++ test-pages/myForm2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form name="register" action="process1.php" method="post">
+
+
+<p>First Name: <input type="text" name="first" /></p>
+<p>Last Name: <input type="text" name="last"></p>
+
+
+
+<p><input type="submit" name="submit" value="Submit Entry" /></p>
+
+
+
+</form>
+
+
+
+</body>
+</html>
---
+++ test-pages/test3.html
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+
+if(isset($_POST['submit'])) {
+	
+	$firstName = $_POST['first'];
+	echo "First Name: " .$firstName ."<br/>";
+
+	$lastName = $_POST['last'];
+	echo "Last Name: " .$lastName ."<br/>";
+	
+
+}
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
